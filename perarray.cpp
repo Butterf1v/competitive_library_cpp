54506842161e996528728c4be71a560f5e4561ae
Butterf1v <butterflvkyopro@gmail.com>
2026-06-12 14:07:45 +0900

    Update perarray.cpp

--- perarray.cpp
+++ perarray.cpp
@@ -1,108 +1,115 @@
 #include <vector>
+#include <algorithm>
 
 template <typename T>
 class perarray {
 private:
-  struct node {
-    node* child[32];
-    T values[32];
-    node() {
-      for (int i = 0; i < 32; ++i) child[i] = nullptr;
-    }
-    node(const node& other) {
-      for (int i = 0; i < 32; ++i) child[i] = other.child[i];
-      for (int i = 0; i < 32; ++i) values[i] = other.values[i];
+  struct Node {
+    int child[4];
+    T val;
+    Node() {
+      child[0] = child[1] = child[2] = child[3] = -1;
+      val = T();
     }
   };
-
-  std::vector<node*> roots;
-  std::vector<node*> all_nodes;
+  std::vector<Node> nodes;
+  std::vector<int> roots;
+  int n;
   int max_shift;
-  int size_;
 
-  node* create_node() {
-    node* n = new node();
-    all_nodes.push_back(n);
-    return n;
-  }
-
-  node* create_node(const node& other) {
-    node* n = new node(other);
-    all_nodes.push_back(n);
-    return n;
-  }
-
-  node* build(int shift, int& idx, const std::vector<T>& vec) {
-    if (idx >= vec.size()) return nullptr;
-    node* res = create_node();
-    if (shift == 0) {
-      for (int i = 0; i < 32 && idx < vec.size(); ++i, ++idx) {
-        res->values[i] = vec[idx];
-      }
-    } else {
-      for (int i = 0; i < 32 && idx < vec.size(); ++i) {
-        res->child[i] = build(shift - 5, idx, vec);
-      }
+  int build(int shift, int idx_base, const std::vector<T>& v) {
+    if (idx_base >= (int)v.size()) return -1;
+    int cur = nodes.size();
+    nodes.emplace_back();
+    if (shift < 0) {
+      nodes[cur].val = v[idx_base];
+      return cur;
     }
+    for (int i = 0; i < 4; ++i) {
+      nodes[cur].child[i] = build(shift - 2, idx_base | (i << shift), v);
+    }
+    return cur;
+  }
+
+  int update(int cur, int shift, int idx, const T& value) {
+    int res = nodes.size();
+    nodes.emplace_back();
+    if (cur != -1) {
+      nodes[res] = nodes[cur];
+    }
+    if (shift < 0) {
+      nodes[res].val = value;
+      return res;
+    }
+    int i = (idx >> shift) & 3;
+    nodes[res].child[i] = update(nodes[res].child[i], shift - 2, idx, value);
     return res;
   }
 
+  const T& get_internal(int ver, int idx) const {
+    int cur = roots[ver];
+    int shift = max_shift;
+    while (shift >= 0 && cur != -1) {
+      int i = (idx >> shift) & 3;
+      cur = nodes[cur].child[i];
+      shift -= 2;
+    }
+    if (cur == -1) {
+      static const T default_value = T();
+      return default_value;
+    }
+    return nodes[cur].val;
+  }
+
 public:
-  perarray(int n) {
-    size_ = n;
-    max_shift = 0;
-    if (size_ > 0) {
-      while ((size_ - 1) >> (max_shift + 5)) {
-        max_shift += 5;
-      }
+  class VersionProxy {
+  private:
+    const perarray& pa;
+    int ver;
+  public:
+    VersionProxy(const perarray& pa, int ver) : pa(pa), ver(ver) {}
+    const T& operator[](int idx) const {
+      return pa.get_internal(ver, idx);
     }
-    std::vector<T> vec(size_);
-    int idx = 0;
-    roots.push_back(build(max_shift, idx, vec));
+  };
+
+  perarray(int elements_count) : n(elements_count) {
+    int shift = 0;
+    while ((1 << shift) < n) {
+      shift += 2;
+    }
+    max_shift = shift;
+    nodes.emplace_back();
+    roots.push_back(0);
   }
 
-  perarray(std::vector<T> init_vec) {
-    size_ = init_vec.size();
-    max_shift = 0;
-    if (size_ > 0) {
-      while ((size_ - 1) >> (max_shift + 5)) {
-        max_shift += 5;
-      }
+  perarray(const std::vector<T>& v) : n(v.size()) {
+    int shift = 0;
+    while ((1 << shift) < n) {
+      shift += 2;
     }
-    int idx = 0;
-    roots.push_back(build(max_shift, idx, init_vec));
-  }
-
-  ~perarray() {
-    for (node* n : all_nodes) {
-      delete n;
+    max_shift = shift;
+    if (n == 0) {
+      nodes.emplace_back();
+      roots.push_back(0);
+    } else {
+      int rt = build(max_shift, 0, v);
+      roots.push_back(rt);
     }
   }
 
-  T get(int version, int index) {
-    node* curr = roots[version];
-    int shift = max_shift;
-    while (shift > 0) {
-      curr = curr->child[(index >> shift) & 0x1f];
-      shift -= 5;
-    }
-    return curr->values[index & 0x1f];
-  }
-
-  int set(int base_version, int index, T val) {
-    node* curr = roots[base_version];
-    node* new_root = create_node(*curr);
-    node* p = new_root;
-    int shift = max_shift;
-    while (shift > 0) {
-      int sub_idx = (index >> shift) & 0x1f;
-      p->child[sub_idx] = create_node(*(curr->child[sub_idx]));
-      p = p->child[sub_idx];
-      curr = curr->child[sub_idx];
-      shift -= 5;
-    }
-    p->values[index & 0x1f] = val;
-    roots.push_back(new_root);
+  int set(int base_version, int index, const T& value) {
+    int old_rt = roots[base_version];
+    int new_rt = update(old_rt, max_shift, index, value);
+    roots.push_back(new_rt);
     return roots.size() - 1;
+  }
+
+  VersionProxy operator[](int ver) const {
+    return VersionProxy(*this, ver);
+  }
+
+  void reserve(size_t capacity) {
+    nodes.reserve(capacity);
   }
 };
